209d62051ac2200971609612f1c33fa9a9024a10
Mickey1299 <807798223@qq.com>
2019-05-29 20:51:49 +0800

    补充了数据校验部分，可以通过浏览器查看数据校验

---
+++ verification-web/src/main/java/com/example/demo/Tvshow.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Tvshow {
+    @NotNull
+    @Min(1)
+    private long id;
+
+    @NotNull
+    @Size(min=3, max=20)
+    private String name;
+
+    @NotNull
+    @Size(min=3, max=10)
+    private String type;
+}
